6b98825880c461facd94f7b3d5fa4af9accd3985
Johan Svensson <johan@exclude.se>
2017-08-29 22:12:59 +0200

    fixed intendentation.

--- Src/SpiHandler.cpp
+++ Src/SpiHandler.cpp
@@ -93,132 +93,132 @@ void SpiHandler::run(){
 	sendData(RXFLUSH, 0x01);
 	HAL_Delay(5);
 
-		for(;;){
+	for(;;){
 
-			//TEST THIS:
-			//WITH INTERRUPT
-			//https://github.com/karlp/Mrf24j40-arduino-library/blob/master/mrf24j.cpp
-			//MRF chipset: http://ww1.microchip.com/downloads/en/DeviceDoc/39776C.pdf
+		//TEST THIS:
+		//WITH INTERRUPT
+		//https://github.com/karlp/Mrf24j40-arduino-library/blob/master/mrf24j.cpp
+		//MRF chipset: http://ww1.microchip.com/downloads/en/DeviceDoc/39776C.pdf
 
-			//Source for init: https://developer.mbed.org/users/hilgo/code/MRF24J40/file/55d2672c4708/MRF24J40.cpp
+		//Source for init: https://developer.mbed.org/users/hilgo/code/MRF24J40/file/55d2672c4708/MRF24J40.cpp
 
-			sendData(MRF_PACON2, 0x98); // – Initialize FIFOEN = 1 and TXONTS = 0x6.
-			sendData(MRF_TXSTBL, 0x95); // – Initialize RFSTBL = 0x9.
+		sendData(MRF_PACON2, 0x98); // – Initialize FIFOEN = 1 and TXONTS = 0x6.
+		sendData(MRF_TXSTBL, 0x95); // – Initialize RFSTBL = 0x9.
 
-			sendLongData(MRF_RFCON0, 0x03); // – Initialize RFOPT = 0x03.
-			sendLongData(MRF_RFCON1, 0x01); // – Initialize VCOOPT = 0x02.
-			sendLongData(MRF_RFCON2, 0x80); // – Enable PLL (PLLEN = 1).
-			sendLongData(MRF_RFCON6, 0x90); // – Initialize TXFIL = 1 and 20MRECVR = 1.
-			sendLongData(MRF_RFCON7, 0x80); // – Initialize SLPCLKSEL = 0x2 (100 kHz Internal oscillator).
-			sendLongData(MRF_RFCON8, 0x10); // – Initialize RFVCO = 1.
-			sendLongData(MRF_SLPCON1, 0x21); // – Initialize CLKOUTEN = 1 and SLPCLKDIV = 0x01.
+		sendLongData(MRF_RFCON0, 0x03); // – Initialize RFOPT = 0x03.
+		sendLongData(MRF_RFCON1, 0x01); // – Initialize VCOOPT = 0x02.
+		sendLongData(MRF_RFCON2, 0x80); // – Enable PLL (PLLEN = 1).
+		sendLongData(MRF_RFCON6, 0x90); // – Initialize TXFIL = 1 and 20MRECVR = 1.
+		sendLongData(MRF_RFCON7, 0x80); // – Initialize SLPCLKSEL = 0x2 (100 kHz Internal oscillator).
+		sendLongData(MRF_RFCON8, 0x10); // – Initialize RFVCO = 1.
+		sendLongData(MRF_SLPCON1, 0x21); // – Initialize CLKOUTEN = 1 and SLPCLKDIV = 0x01.
 
-			//  Configuration for nonbeacon-enabled devices (see Section 3.8 “Beacon-Enabled and
-			//  Nonbeacon-Enabled Networks”):
-			sendData(MRF_BBREG2, 0x80); // Set CCA mode to ED
-			sendData(MRF_CCAEDTH, 0x60); // – Set CCA ED threshold.
-			sendData(MRF_BBREG6, 0x40); // – Set appended RSSI value to RXFIFO.
-
-
-			//set_interrupts();
-			sendData(MRF_INTCON, 0b11110110);
-			uint8_t channel = 12;
-			//set_channel(12);
-			sendData(MRF_RFCON0, (((channel - 11) << 4) | 0x03));
+		//  Configuration for nonbeacon-enabled devices (see Section 3.8 “Beacon-Enabled and
+		//  Nonbeacon-Enabled Networks”):
+		sendData(MRF_BBREG2, 0x80); // Set CCA mode to ED
+		sendData(MRF_CCAEDTH, 0x60); // – Set CCA ED threshold.
+		sendData(MRF_BBREG6, 0x40); // – Set appended RSSI value to RXFIFO.
 
 
-			// max power is by default.. just leave it...
-			// Set transmitter power - See “REGISTER 2-62: RF CONTROL 3 REGISTER (ADDRESS: 0x203)”.
-			sendData(MRF_RFCTL, 0x04); //  – Reset RF state machine.
-			sendData(MRF_RFCTL, 0x00); // part 2
+		//set_interrupts();
+		sendData(MRF_INTCON, 0b11110110);
+		uint8_t channel = 12;
+		//set_channel(12);
+		sendData(MRF_RFCON0, (((channel - 11) << 4) | 0x03));
 
 
-			recvData(MRF_INTCON);
-			osDelay(1); // delay at least 192usec
-
-			// Reset RF module.
-			sendData(RFCTL, 0x04);
-			sendData(RFCTL, 0x00);
-			sendData(RFCTL, 0x00);
-
-			sendData(PANIDL, 0xAA);
-			sendData(PANIDH, 0xAA);
-			sendData(SADRL, 0xAA);
-			sendData(SADRH, 0xAA);
-
-			// Flush RX fifo.
-
-			recvData(RXFLUSH);
+		// max power is by default.. just leave it...
+		// Set transmitter power - See “REGISTER 2-62: RF CONTROL 3 REGISTER (ADDRESS: 0x203)”.
+		sendData(MRF_RFCTL, 0x04); //  – Reset RF state machine.
+		sendData(MRF_RFCTL, 0x00); // part 2
 
 
-			// Write MAC addresses here. We don't care.
+		recvData(MRF_INTCON);
+		osDelay(1); // delay at least 192usec
 
-			sendLongData(RFCTRL2, 0x80);  // Enable RF PLL.
+		// Reset RF module.
+		sendData(RFCTL, 0x04);
+		sendData(RFCTL, 0x00);
+		sendData(RFCTL, 0x00);
 
-			sendLongData(RFCTRL3, 0x00);  // Full power.
-			sendLongData(RFCTRL6, 0x80);  // Enable TX filter (recommended)
-			sendLongData(RFCTRL8, 0x10);  // Enhanced VCO (recommended)
+		sendData(PANIDL, 0xAA);
+		sendData(PANIDH, 0xAA);
+		sendData(SADRL, 0xAA);
+		sendData(SADRH, 0xAA);
 
-			sendData(BBREG2,0x78);   // Clear Channel Assesment use carrier sense.
-			sendData(BBREG6,0x40);   // Calculate RSSI for Rx packet.
-			sendData(RSSITHCCA,0x00);// RSSI threshold for CCA.
+		// Flush RX fifo.
 
-			sendLongData(RFCTRL0, 0x00);  // Channel 11.
-
-			readLongData(RFCTRL0);
-
-			sendData(RXMCR, 0x01); // Don't check address upon reception.
-			//MrfWriteShort(RXMCR, 0x00); // Check address upon reception.
-			//
-			//// Reset RF module with new settings.
-			sendData(RFCTL, 0x04);
-			sendData(RFCTL, 0x00);
-			// END OF MBED CODE
+		recvData(RXFLUSH);
 
 
-            //
-		    //uint8_t data[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a };
-		    //uint8_t i;
-		    //uint8_t length = 11;
-            //
-		    //sendLongData(0x000, 0);   // No addresses in header.
-		    //sendLongData(0x001, length); // 11 bytes
-		    //for(i=0; i<length; i++)
-		    //	sendLongData(0x002+i, data[i]);
-            //
-		    //recvData(TXNMTRIG);
-		    //sendData(TXNMTRIG, 0x01);
-		    //recvData(TXNMTRIG);
-		    //recvData(TXNMTRIG);
-		    //recvData(TXNMTRIG);
+		// Write MAC addresses here. We don't care.
+
+		sendLongData(RFCTRL2, 0x80);  // Enable RF PLL.
+
+		sendLongData(RFCTRL3, 0x00);  // Full power.
+		sendLongData(RFCTRL6, 0x80);  // Enable TX filter (recommended)
+		sendLongData(RFCTRL8, 0x10);  // Enhanced VCO (recommended)
+
+		sendData(BBREG2,0x78);   // Clear Channel Assesment use carrier sense.
+		sendData(BBREG6,0x40);   // Calculate RSSI for Rx packet.
+		sendData(RSSITHCCA,0x00);// RSSI threshold for CCA.
+
+		sendLongData(RFCTRL0, 0x00);  // Channel 11.
+
+		readLongData(RFCTRL0);
+
+		sendData(RXMCR, 0x01); // Don't check address upon reception.
+		//MrfWriteShort(RXMCR, 0x00); // Check address upon reception.
+		//
+		//// Reset RF module with new settings.
+		sendData(RFCTL, 0x04);
+		sendData(RFCTL, 0x00);
+		// END OF MBED CODE
+
+
+		//
+		//uint8_t data[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a };
+		//uint8_t i;
+		//uint8_t length = 11;
+		//
+		//sendLongData(0x000, 0);   // No addresses in header.
+		//sendLongData(0x001, length); // 11 bytes
+		//for(i=0; i<length; i++)
+		//	sendLongData(0x002+i, data[i]);
+		//
+		//recvData(TXNMTRIG);
+		//sendData(TXNMTRIG, 0x01);
+		//recvData(TXNMTRIG);
+		//recvData(TXNMTRIG);
+		//recvData(TXNMTRIG);
 
 
 
 
-//			  osDelay(20);
-			  //HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
-			  //HAL_GPIO_WritePin(MRF_CS_GPIO_Port, MRF_CS_Pin, GPIO_PIN_SET);
-			  //HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
-			  //HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, bufferSize, 50);
-			//if(!mCallback){
-				//reset();
-			//sendData(0x2a, 0x07); //SOFTRST
-			//sendData(0x18, 0x98); //TXSTBL
-			//sendData(0x2e, 0x95); //Initialize RFSTBL = 0x9.
-			//sendLongData(0x200, 0x03);
-			//sendLongData(0x201, 0x01);
-			//sendLongData(0x202, 0x80);
-			//sendLongData(0x206, 0x90);
-			//sendLongData(0x207, 0x80);
-			//sendLongData(0x208, 0x10);
-			//sendLongData(0x220, 0x21);
-            //
-            //
-			//sendData(0x41, 0x03); //RFCON0
-			//sendLongData(0x301, 0x01);
-			//sendData(); //RFCON1
+		//			  osDelay(20);
+		//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
+		//HAL_GPIO_WritePin(MRF_CS_GPIO_Port, MRF_CS_Pin, GPIO_PIN_SET);
+		//HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
+		//HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, bufferSize, 50);
+		//if(!mCallback){
+		//reset();
+		//sendData(0x2a, 0x07); //SOFTRST
+		//sendData(0x18, 0x98); //TXSTBL
+		//sendData(0x2e, 0x95); //Initialize RFSTBL = 0x9.
+		//sendLongData(0x200, 0x03);
+		//sendLongData(0x201, 0x01);
+		//sendLongData(0x202, 0x80);
+		//sendLongData(0x206, 0x90);
+		//sendLongData(0x207, 0x80);
+		//sendLongData(0x208, 0x10);
+		//sendLongData(0x220, 0x21);
+		//
+		//
+		//sendData(0x41, 0x03); //RFCON0
+		//sendLongData(0x301, 0x01);
+		//sendData(); //RFCON1
 
-			/*
+		/*
 			1. SOFTRST (0x2A) = 0x07 – Perform a software Reset. The bits will be automatically cleared to ‘0’ by hardware.
 			2. PACON2 (0x18) = 0x98 – Initialize FIFOEN = 1 and TXONTS = 0x6.
 			3. TXSTBL (0x2E) = 0x95 – Initialize RFSTBL = 0x9.
@@ -229,20 +229,20 @@ void SpiHandler::run(){
 			8. RFCON7 (0x207) = 0x80 – Initialize SLPCLKSEL = 0x2 (100 kHz Internal oscillator).
 			9. RFCON8 (0x208) = 0x10 – Initialize RFVCO = 1.
 			10. SLPCON1 (0x220) = 0x21 – Initialize CLKOUTEN = 1 and SLPCLKDIV = 0x01
-*/
+		 */
 
-			//sendData(0x3a, 0x80);
-			//sendData(0x3F, 0x60);
-			//sendData(0x3e, 0x40);
-			//sendData(0x36, 0x04);
-			//recvData(0x36);
-			//sendData(0x36,0x00);
-			//recvData(0x36);
-			//recvData(0x3a);
-			osDelay(20);
+		//sendData(0x3a, 0x80);
+		//sendData(0x3F, 0x60);
+		//sendData(0x3e, 0x40);
+		//sendData(0x36, 0x04);
+		//recvData(0x36);
+		//sendData(0x36,0x00);
+		//recvData(0x36);
+		//recvData(0x3a);
+		osDelay(20);
 
 
-			/*
+		/*
 			Configuration for nonbeacon-enabled devices (see Section 3.8 “Beacon-Enabled and Nonbeacon-Enabled
 			Networks”):
 			11. BBREG2 (0x3A) = 0x80 – Set CCA mode to ED.
@@ -259,66 +259,66 @@ void SpiHandler::run(){
 17. RFCTL (0x36) = 0x04 – Reset RF state machine.
 18. RFCTL (0x36) = 0x00.
 19. Delay at least 192 μs.
-			*/
+		 */
 
 
-			  //HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, bufferSize, 50);
+		//HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, bufferSize, 50);
 
 
-			  //osDelay(20);
+		//osDelay(20);
 
 
-//			  aTxBuffer = ~aTxBuffer;
-//			  //HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
-//			  HAL_GPIO_WritePin(MRF_CS_GPIO_Port, MRF_CS_Pin, GPIO_PIN_RESET);
-//			  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-//			  HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, bufferSize, 50);
-		}
+		//			  aTxBuffer = ~aTxBuffer;
+		//			  //HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+		//			  HAL_GPIO_WritePin(MRF_CS_GPIO_Port, MRF_CS_Pin, GPIO_PIN_RESET);
+		//			  HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+		//			  HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, bufferSize, 50);
+	}
 
 
 	/* USER CODE BEGIN 2 */
 	//  uint8_t data = 0xA5;
-	  //HAL_SPI_Init(mSpi_handler);
-	  //uint8_t return_transmit;
-//	  uint8_t aTxBuffer = 0xF0;
-//	  uint8_t aRxBuffer = 0x20;
-//	  int const BUFFERSIZE = 1;
+	//HAL_SPI_Init(mSpi_handler);
+	//uint8_t return_transmit;
+	//	  uint8_t aTxBuffer = 0xF0;
+	//	  uint8_t aRxBuffer = 0x20;
+	//	  int const BUFFERSIZE = 1;
 
-	  //uint32_t i = 0;
-	  //uint8_t SPI1_Tx_Data  = 0xD000;
-	  //uint8_t  SPI1_Rx_Data  = 0;
-//	for(;;)
-//	{
-//		  //HAL_Delay(50);
-//		osDelay(100);
-//		  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
-//		  HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, BUFFERSIZE, 50);
-//		  HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, BUFFERSIZE, 50);
-//			osDelay(100);
-//			  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
-//		  		  //HAL_Delay(200);
-		  //HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
-		//  HAL_SPI_Transmit(mSpi_handler, &data, BUFFERSIZE, 15);
-		//HAL_SPI_Transmit()
-		  //data = ~data;
-		  //osDelay(20);
-		  //HAL_Delay(200);
-		  //HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
-		  //HAL_SPI_Transmit(&hspi3, &data, BUFFERSIZE, 15);
-		  //return_transmit = HAL_SPI_Transmit(&hspi2, &data, 1, 500);
-		  //HAL_Delay(50);
-		  //HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
-		//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
-
-
+	//uint32_t i = 0;
+	//uint8_t SPI1_Tx_Data  = 0xD000;
+	//uint8_t  SPI1_Rx_Data  = 0;
+	//	for(;;)
+	//	{
+	//		  //HAL_Delay(50);
+	//		osDelay(100);
+	//		  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
+	//		  HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, BUFFERSIZE, 50);
+	//		  HAL_SPI_TransmitReceive(mSpi_handler, &aTxBuffer, &aRxBuffer, BUFFERSIZE, 50);
+	//			osDelay(100);
+	//			  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+	//		  		  //HAL_Delay(200);
+	//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
+	//  HAL_SPI_Transmit(mSpi_handler, &data, BUFFERSIZE, 15);
+	//HAL_SPI_Transmit()
+	//data = ~data;
+	//osDelay(20);
+	//HAL_Delay(200);
+	//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+	//HAL_SPI_Transmit(&hspi3, &data, BUFFERSIZE, 15);
+	//return_transmit = HAL_SPI_Transmit(&hspi2, &data, 1, 500);
+	//HAL_Delay(50);
+	//HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_RESET);
+	//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
 
 
 
 
 
 
-		//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
-		//osDelay(1);
+
+
+	//HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET);
+	//osDelay(1);
 	//}
 }
 
